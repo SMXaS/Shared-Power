feae2818f988ff703b383c0ac2e310ed88e8c475
kvarcas91 <e.slutas@gmail.com>
2018-12-15 16:17:40 +0000

    addTool adding functionality 1/2

--- addTool.py
+++ addTool.py
@@ -1,4 +1,6 @@
 from Entities.Tool import Tool
+import util
+import WriteFile as wf
 
 
 class addTool:
@@ -7,9 +9,11 @@ class addTool:
         self.__owner = owner
 
     def add(self, item):
-        ID = "myLovelyItemID"
+        ID = util.generateID()
         toolOwner = self.__owner.getUserName()
         #toolOwner = "kvarcas91"
+        
+        """
         title = "ToolName1"
         description = "Where's Stefan 1"
         dayPrice = 9.89
@@ -18,11 +22,14 @@ class addTool:
         available = True
 
         tool = Tool(ID, toolOwner, title, description, dayPrice, halfDayPrice, imagePath, available)
+        """
+       
+        isCorrect = util.verifyTool(item)
+        if isCorrect:
+            print("Tool has been added")
 
-        self.printTool(tool)
-        # Do something with "tool" object
-
+        """
     def printTool(self, tool):
         print(tool.getOwner())
-
+        """
 
