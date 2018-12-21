c02a7da2f4c3041f87f51aeba53d7da93453446f
kvarcas91 <e.slutas@gmail.com>
2018-12-21 12:17:38 +0000

    Update Bookings.py

--- Entities/Bookings.py
+++ Entities/Bookings.py
@@ -1,1 +1,26 @@
 class Bookings:
+  
+    def __init__ (self, toolID, userName, startDate, expectedReturnDate, returnDate = ""):
+        self.toolID = toolID
+        self.userName = userName
+        self.startDate = startDate
+        self.expectedReturnDate = expectedReturnDate
+        self.returnDate = returnDate
+    
+    def getToolID(self):
+        return self.toolID
+    
+    def getUserName(self):
+        return self.userName
+      
+    def getStartDate(self):
+        return self.startDate
+      
+    def getExpectedReturnDate(self):
+        return self.expectedReturnDate
+      
+    def get returnDate(self):
+        return self.returnDate
+      
+    def setReturnDate(self, returnDate):
+        self.returnDate = returnDate
