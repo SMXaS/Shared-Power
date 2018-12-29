ac5a5945eb8bf16ae4483815ace798d8ca6f0045
eslut <e.slutas@gmail.com>
2018-12-29 20:39:00 +0000

    fixed a bug with search tool

--- Code/Utilities/ReadFile.py
+++ Code/Utilities/ReadFile.py
@@ -15,7 +15,7 @@ def getTool(returnObj, column, value):
     with open("Data/tools.csv", 'r') as f:
         l = list(csv.reader(f))
         my_dict = {i[0]: [x for x in i[1:]] for i in zip(*l)}
-        item = [i for i, x in enumerate(my_dict[column]) if value == x]
+        item = [i for i, x in enumerate(my_dict[column]) if value in x.lower()]
 
     if returnObj:
         itemList = []
