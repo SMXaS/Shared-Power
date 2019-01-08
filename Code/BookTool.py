73fc55a5034587d6acd6f6e18b5a10589ad4c01e
AdamTakaacs <45607765+AdamTakaacs@users.noreply.github.com>
2019-01-08 21:20:20 +0000

    Documentation for BookTool

--- Code/BookTool.py
+++ Code/BookTool.py
@@ -5,7 +5,37 @@ import Code.Utilities.WriteFile as wf
 import Code.Utilities.ReadFile as rf
 import uuid
 
+"""
+-------------------------------------------------------------------------------------------------
+This Class Makes the Booking For the user
+-------------------------------------------------------------------------------------------------
 
+---------------------------------------------------------------------
+*** Implementation:
+---------------------------------------------------------------------
+    class:
+        @ 'your assigned name' = BookTool(availableDateBox, availableEndDateBox, tool):
+            * takes the start date for the tool
+            * takes the end date for the tool
+            * takes the info of the tool
+---------------------------------------------------------------------
+    methods:
+        @ 'your assigned name'.getNextDays():
+            - sends back the available end Dates to UI
+        @ 'your assigned name'.getStartDate():
+            - gets the Start Date 
+            - based on Start Date will generate End Dates and populates the List
+        @ 'your assigned name'.populateStartList():
+            -Fills the Start booking list with available Dates
+        @ 'your assigned name'.getEndDate():
+            -Gets the end Date of the booking from the User
+        @ 'your assigned name'.getMessage():
+            -sends back the message to the UI
+        @ 'your assigned name'.hireTool(login, startTerm, endTerm, pickUpEntry, dropOffEntry):
+            -verifies booking if everything is ok
+        @ 'your assigned name'.vfWrite():
+            -creates a booking Object and passes to writefile
+"""
 
 class BookTool:
     def __init__(self, availableDateBox, availableEndDateBox, tool):
@@ -17,12 +47,6 @@ class BookTool:
         return self.nextDays
 
     def getStartDate(self):
-        """
-        Getting start booking date.
-        based on selection from start date (bookings) list will generate end date list
-
-        :return: None
-        """
 
         index = int(self.availableDateBox.curselection()[0])
         self.__start_date = self.availableDateBox.get(index)
@@ -30,29 +54,24 @@ class BookTool:
         self.nextDays = util.getNextAvailableDates(self.__start_date, self.availableDateList)
 
 
-        self.populateEndList()
-
+        self.__populateEndList()
 
     def populateStartList(self):
-        """
-        Fills Start booking list with available dates
-        :return: None
-        """
 
-        bookingList = self.getBookingList()
+        bookingList = self.__getBookingList()
 
-        self.availableDateList = self.getAvailableList(bookingList)
+        self.availableDateList = self.__getAvailableList(bookingList)
         for i in range(len(self.availableDateList)):
             self.availableDateBox.insert("end", self.availableDateList[i])
 
-    def getBookingList(self):
+    def __getBookingList(self):
         """
         :return: list(obj(all bookings for particular item))
         """
 
         return rf.getAllBookings("toolID", self.tool.getID(), 0)
 
-    def getAvailableList(self, bookingList):
+    def __getAvailableList(self, bookingList):
         """
         :param bookingList: list(obj(bookings))
         :return: list(available dates)
@@ -60,9 +79,7 @@ class BookTool:
 
         return util.getBookingDates(bookingList)
 
-
-
-    def populateEndList(self):
+    def __populateEndList(self):
         """
           Fills Return booking list with available dates
           :return: None
@@ -73,43 +90,29 @@ class BookTool:
             print(self.nextDays[i])
             self.availableEndDateBox.insert("end", self.nextDays[i])
 
-
     def getEndDate(self):
-        """
-        Getting end booking date. Calls "show arrange rider" function
-        :return: None
-        """
 
         index = int(self.availableEndDateBox.curselection()[0])
         self.__end_date = self.availableEndDateBox.get(index)
 
-    def verifyHiring(self):
+    def __verifyHiring(self):
         """
         Verifies booking
         :return: boolean (True - approved, False - not)
         """
 
-        booking = self.getBookingList()
-        availabeList = self.getAvailableList(booking)
+        booking = self.__getBookingList()
+        availabeList = self.__getAvailableList(booking)
         dayDiff = util.getDayDifference(self.__start_date, self.__end_date)
         if util.verifyBooking(self.__start_date, availabeList, dayDiff):
             return True
         else:
             return False
 
-
     def getMessage(self):
         return self.message
 
     def hireTool(self, login, startTerm, endTerm, pickUpEntry, dropOffEntry):
-        """
-        Verify booking. If True:
-            Create booking object and pass it to WriteFile
-            Will return to SearchToolPage
-        :return: None
-        """
-
-
 
         if self.__start_date and self.__end_date:
             self.hiredTool = Bookings(uuid.uuid4(), self.tool.getID(), login, self.tool.getCondition(),
@@ -119,7 +122,7 @@ class BookTool:
             self.hiredTool.setPickUpLocation(pickUpEntry)
             self.hiredTool.setDropOffLocation(dropOffEntry)
 
-            if self.verifyHiring():
+            if self.__verifyHiring():
                 # show confirmation window
                 totalPrice = util.calculateToolhireCost(self.hiredTool, self.tool)
                 self.message = "{} {}\n{} {}\n{} {}\n{} {}{}\n\n{}".format(strings.toolTitleForInvoice, self.tool.getTitle(),
@@ -132,9 +135,6 @@ class BookTool:
                 return 1
             else:
                 return 0
-
-
-
 
         else:
             print("Error")
